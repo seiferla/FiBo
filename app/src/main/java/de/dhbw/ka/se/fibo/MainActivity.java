7e67da09ed5b62936cf47bf110093be46a2338bb
Markus Götz <markus.goetz17@gmail.com>
2022-10-06 11:22:02 +0200

    initial commit

---
+++ app/src/main/java/de/dhbw/ka/se/fibo/MainActivity.java
@@ -0,0 +1,13 @@
+package de.dhbw.ka.se.fibo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
