b939cafabd3428cbf22c186f67e5575e84a9dd41
jens1o <mail@jens-hausdorf.de>
2022-11-04 09:57:41 +0100

    Created Splash screen and embedded icons #FiBo-64 Development 45m

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/MainActivity.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/MainActivity.java
@@ -3,6 +3,7 @@ package de.dhbw.ka.se.fibo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/SplashActivity.java
@@ -0,0 +1,37 @@
+package de.dhbw.ka.se.fibo;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen") // We need a custom splash screen because we want to support Android versions below 11
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(4000);
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
