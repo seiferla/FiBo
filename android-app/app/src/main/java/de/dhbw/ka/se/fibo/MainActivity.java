384a7be7293377e260ef3bc790a2fa1b5e4bb104
Jonas-Karl <jonas.karl.rade@gmail.com>
2022-11-09 00:42:39 +0100

    changed the MainActivity functionality so that we can use the navigation bar

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/MainActivity.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/MainActivity.java
@@ -1,50 +1,40 @@
 package de.dhbw.ka.se.fibo;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import de.dhbw.ka.se.fibo.adapters.CashflowAdapter;
-import de.dhbw.ka.se.fibo.models.Cashflow;
-import de.dhbw.ka.se.fibo.models.CashflowType;
+import de.dhbw.ka.se.fibo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Helpers.updateSupportActionBarText(
                 getApplicationContext(),
                 getSupportActionBar(),
                 getText(R.string.app_activity_main_title)
         );
 
-        final ListView list = findViewById(R.id.list);
-        ArrayList<Cashflow> arrayList = new ArrayList<>();
+        //The part below is for navigationBar functionality (swapping pages)
 
-        arrayList.add(new Cashflow(CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(CashflowType.INCOME, BigDecimal.valueOf(120.5), new Date(), "Gehalt"));
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
 
-        ArrayAdapter<Cashflow> listAdapter = new CashflowAdapter(this, android.R.layout.simple_list_item_1, arrayList);
-        list.setAdapter(listAdapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Cashflow clickedItem = (Cashflow) list.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, clickedItem.getOverallValue().toString(), Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
