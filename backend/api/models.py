d095c902f995b1cbf9c50877626766e4aac58e16
XSnelliusX <57440280+XSnelliusX@users.noreply.github.com>
2022-11-22 13:02:13 +0100

    add first entities #FiBo-24

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length=55, primary_key=True, unique=True)
+    email = models.CharField(max_length=55, unique=True)
+    password = models.CharField(max_length=55)
+
+
+class Account(models.Model):
+    account_id = models.CharField(max_length=55, primary_key=True)
+
+
+class Cashflow(models.Model):
+    cashflow_id = models.UUIDField(primary_key=True)
+    is_income = models.BooleanField()
+    overall_value = models.FloatField()
+    timestamp = models.DateTimeField()
+
+
+class Item(models.Model):
+    item_id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=55)
+    amount = models.FloatField()
+    value = models.FloatField()
+
+
+class Category(models.Model):
+    category_id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=55)
+
+
+class Place(models.Model):
+    address = models.CharField(max_length=55, primary_key=True)
+    name = models.CharField(max_length=55)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import psycopg2.extensions
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ibg9w7j6qg6&t+5554e1$y6nv6$x-oipja+4q==9t5=7q3js2'
+# Your secret key
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "api",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -71,17 +77,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.postgresql',
-        'OPTIONS': {
-            'service': 'my_service',
-            'passfile': '.my_pgpass',
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
