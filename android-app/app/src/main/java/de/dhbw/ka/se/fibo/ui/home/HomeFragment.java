93f323923a83349db2a67145b1c8836a55f527ea
Lars Seifert <larswilhelm.seifert@dm.de>
2022-11-30 18:58:48 +0100

    update view

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/home/HomeFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/home/HomeFragment.java
@@ -1,7 +1,6 @@
 package de.dhbw.ka.se.fibo.ui.home;
 
 
-import android.gesture.GestureOverlayView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +8,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 
 import de.dhbw.ka.se.fibo.R;
 import de.dhbw.ka.se.fibo.databinding.FragmentHomeBinding;
@@ -19,16 +21,18 @@ import de.dhbw.ka.se.fibo.models.Cashflow;
 import de.dhbw.ka.se.fibo.models.CashflowType;
 import de.dhbw.ka.se.fibo.models.Category;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     ArrayList<Cashflow> arrayList = new ArrayList<>();
     private RecyclerView recyclerView;
 
+
+    public HomeFragment() {
+        super();
+        initializeData();
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,21 +45,14 @@ public class HomeFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.setAdapter(new ListAdapter(getContext(), arrayList));
 
-        initializeData();
         return view;
     }
 
     private void initializeData() {
         arrayList.add(new Cashflow(Category.RESTAURANT, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.GESCHENK, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.MOBILITÄT, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.SONSTIGE, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.BILDUNG, CashflowType.INCOME, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.RESTAURANT, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.GESCHENK, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.KULTUR, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.SOZIALLEBEN, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "dm"));
-        arrayList.add(new Cashflow(Category.BILDUNG, CashflowType.INCOME, BigDecimal.valueOf(12.5), new Date(), "dm"));
+        arrayList.add(new Cashflow(Category.GESCHENK, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), new Date(), "lidl"));
+        arrayList.add(new Cashflow(Category.MOBILITÄT, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), new Date(), "kaufland"));
+
     }
 
     @Override
