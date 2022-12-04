1cab0c461d838499a84c1767c5840fb7c7cb819d
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2022-12-04 22:17:22 +0100

    initial commit

---
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragment.java
@@ -0,0 +1,19 @@
+package de.dhbw.ka.se.fibo.ui.adding;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
+
+public class AddingFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    // action_navigation_adding_to_navigation_home
+}
