1d37e32301b5a661af0c0c76f2361a8c545c05fa
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2022-12-07 19:36:10 +0100

    changed format of adding fragment

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragment.java
@@ -1,26 +1,62 @@
 package de.dhbw.ka.se.fibo.ui.adding;
 
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
+import de.dhbw.ka.se.fibo.Helpers;
 import de.dhbw.ka.se.fibo.R;
+import de.dhbw.ka.se.fibo.databinding.FragmentAddingBinding;
+import de.dhbw.ka.se.fibo.databinding.FragmentHomeBinding;
 
 public class AddingFragment extends Fragment {
+
+    private FragmentAddingBinding binding;
+    private MaterialDatePicker<Long> datePicker;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_adding, container, false);
+        binding = FragmentAddingBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        ImageButton datePickerButton = binding.datePickerButton;
+        createDatePicker();
+        binding.dateTextfield.setEnabled(false);
+        datePickerButton.setOnClickListener(v -> datePicker.show(requireActivity().getSupportFragmentManager(), "datePick"));
         return view;
     }
-    // action_navigation_adding_to_navigation_home
+
+    private void createDatePicker() {
+        datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build();
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                Format formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
+                binding.dateTextfield.setText(formatter.format(selection));
+            }
+        });
+    }
 }
