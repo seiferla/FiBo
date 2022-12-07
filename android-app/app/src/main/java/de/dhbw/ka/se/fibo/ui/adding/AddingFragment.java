fadff74c8adf357490208658f07b925f4daa56d3
Jonas-Karl <jonas.karl.rade@gmail.com>
2022-12-08 00:40:12 +0100

    refactor ImageButton to Button

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragment.java
@@ -1,33 +1,28 @@
 package de.dhbw.ka.se.fibo.ui.adding;
 
 import android.os.Bundle;
-import android.provider.SyncStateContract;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 
-import de.dhbw.ka.se.fibo.Helpers;
-import de.dhbw.ka.se.fibo.R;
 import de.dhbw.ka.se.fibo.databinding.FragmentAddingBinding;
-import de.dhbw.ka.se.fibo.databinding.FragmentHomeBinding;
 
 public class AddingFragment extends Fragment {
 
@@ -40,7 +35,7 @@ public class AddingFragment extends Fragment {
 
         binding = FragmentAddingBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        ImageButton datePickerButton = binding.datePickerButton;
+        Button datePickerButton = binding.datePickerButton;
         createDatePicker();
         binding.dateTextField.setEnabled(false);
         datePickerButton.setOnClickListener(v -> datePicker.show(requireActivity().getSupportFragmentManager(), "datePick"));
