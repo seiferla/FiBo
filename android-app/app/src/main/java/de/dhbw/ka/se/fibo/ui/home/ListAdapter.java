7ce89e36bc64c87a20c25806775dc7f9973abcb0
Jonas-Karl <jonas.karl.rade@gmail.com>
2022-12-09 13:15:05 +0100

    Apply format

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/home/ListAdapter.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/home/ListAdapter.java
@@ -63,7 +63,7 @@ public class ListAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
 
     //Todo implement method
     private void openDetailsPage(MaterialCardView materialCardView, Cashflow cashflow, int position) {
-       Toast.makeText(context, position + ". " + cashflow.getPlace().getName() + " " + cashflow.getType().getSign() + Helpers.formatBigDecimalCurrency(cashflow.getOverallValue()), Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, position + ". " + cashflow.getPlace().getName() + " " + cashflow.getType().getSign() + Helpers.formatBigDecimalCurrency(cashflow.getOverallValue()), Toast.LENGTH_SHORT).show();
     }
 
     @Override
