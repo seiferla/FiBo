d5189694af8991c6700f3906b1f2c9388e0f06c5
jens1o <mail@jens-hausdorf.de>
2022-12-11 13:10:17 +0100

    Merged from master #FiBo-115 work Development 15m

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ApplicationState.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ApplicationState.java
@@ -12,6 +12,7 @@ import java.util.TreeSet;
 import de.dhbw.ka.se.fibo.models.Cashflow;
 import de.dhbw.ka.se.fibo.models.CashflowType;
 import de.dhbw.ka.se.fibo.models.Category;
+import de.dhbw.ka.se.fibo.models.Place;
 
 public class ApplicationState {
 
@@ -27,10 +28,10 @@ public class ApplicationState {
         this.context = context;
 
         cashflows = new TreeSet<>();
-        cashflows.add(new Cashflow(Category.RESTAURANT, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), LocalDateTime.now(), "dm"));
-        cashflows.add(new Cashflow(Category.HEALTH, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), LocalDateTime.now().minusDays(5), "lidl"));
-        cashflows.add(new Cashflow(Category.SOCIALLIFE, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), LocalDateTime.now().minusDays(2), "Club"));
-        cashflows.add(new Cashflow(Category.CULTURE, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), LocalDateTime.now().minusDays(1), "ZKM"));
+        cashflows.add(new Cashflow(Category.RESTAURANT, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), LocalDateTime.now(), new Place("dm", 124, "Am dm-Platz 1")));
+        cashflows.add(new Cashflow(Category.HEALTH, CashflowType.EXPENSE, BigDecimal.valueOf(12.5), LocalDateTime.now().minusDays(5), new Place("kaufland", 243, "Kaufplatz")));
+        cashflows.add(new Cashflow(Category.SOCIALLIFE, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), LocalDateTime.now().minusDays(2), new Place("Club", 2, "In da club street")));
+        cashflows.add(new Cashflow(Category.CULTURE, CashflowType.EXPENSE, BigDecimal.valueOf(13.5), LocalDateTime.now().minusDays(1), new Place("ZKM", 32, "Lorenzstraße 19, 76135 Karlsruhe")));
     }
 
     public static ApplicationState getInstance(Context context) {
