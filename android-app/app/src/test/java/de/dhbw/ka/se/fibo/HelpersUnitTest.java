3e603adcb1854ad5b1213a51e2ad3d8c5b08b321
Cebox82 <seifert_lars@gmx.net>
2023-04-13 09:22:49 +0200

    deleted method and renamed unit test file

--- android-app/app/src/test/java/de/dhbw/ka/se/fibo/HelpersUnitTest.java
+++ android-app/app/src/test/java/de/dhbw/ka/se/fibo/HelpersUnitTest.java
@@ -12,11 +12,7 @@ import java.math.BigDecimal;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
-  @Test
-  public void addition_isCorrect() {
-    assertEquals(4, 2 + 2);
-  }
+  
 
   @Test
   public void testFormatBigDecimalCurrency(){
