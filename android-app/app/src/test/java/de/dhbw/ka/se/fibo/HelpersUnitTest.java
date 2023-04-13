6f2575067a578e0078b232ac2a7e3989b782aff6
Cebox82 <seifert_lars@gmx.net>
2023-04-13 16:46:45 +0200

    changed class name

--- android-app/app/src/test/java/de/dhbw/ka/se/fibo/HelpersUnitTest.java
+++ android-app/app/src/test/java/de/dhbw/ka/se/fibo/HelpersUnitTest.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class HelpersUnitTest {
   
 
   @Test
