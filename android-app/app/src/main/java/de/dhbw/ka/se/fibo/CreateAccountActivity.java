d82b3530c4b8cd0ad199ad697e49a4b3ebbb6cde
Cebox82 <seifert_lars@gmx.net>
2023-04-18 16:25:17 +0200

    createdUser Method

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/CreateAccountActivity.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/CreateAccountActivity.java
@@ -3,27 +3,35 @@ package de.dhbw.ka.se.fibo;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.webkit.HttpAuthHandler;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Locale;
 
-import de.dhbw.ka.se.fibo.databinding.ActivityMainBinding;
 import de.dhbw.ka.se.fibo.databinding.CreateAccountBinding;
 
 public class CreateAccountActivity extends AppCompatActivity {
     private CreateAccountBinding binding;
-
     private MaterialButton registerButton;
 
     private Thread buttonClickThread;
+
+    private static final String post_url = "http://127.0.0.1:8000/users/register/";
+    private TextInputEditText create_account_email;
+
+    private TextInputEditText create_account_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +65,23 @@ public class CreateAccountActivity extends AppCompatActivity {
     private void registerButtonClicked() {
         Intent i = new Intent(CreateAccountActivity.this,
                 MainActivity.class);
+        create_account_email = findViewById(R.id.create_account_email);
+        String email = create_account_email.getText().toString();
+
+        create_account_password = findViewById(R.id.create_account_password);
+        String password = create_account_password.getText().toString();
+        
+        createUser(email, password);
         startActivity(i);
         finish();
+    }
+
+    private void createUser(String email, String password) {
+
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
