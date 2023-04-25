f703bd641e367cf85234df57214a66f8f1a99b0f
ChrisEwert <jc01.ewert@gmail.com>
2023-04-25 16:34:25 +0200

    Run Espresso Test

--- android-app/app/src/androidTest/java/de/dhbw/ka/se/fibo/datePicker/DatePickerTest.java
+++ android-app/app/src/androidTest/java/de/dhbw/ka/se/fibo/datePicker/DatePickerTest.java
@@ -1,5 +1,33 @@
 package de.dhbw.ka.se.fibo.datePicker;
 
-public class DatePickerTest {
+import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+import java.io.IOException;
+
+import de.dhbw.ka.se.fibo.CreateAccountActivity;
+
+public class DatePickerTest {
+//    @Rule
+//    public ActivityScenarioRule<CreateAccountActivity> activityScenarioRule
+//            = new ActivityScenarioRule<>(CreateAccountActivity.class);
+//
+//    private MockWebServer server = new MockWebServer();
+//
+//    private Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//    @Before
+//    public void setUp() throws IOException {
+//        server.start(8000);
+//    }
+//
+//    @After
+//    public void tearDown() throws IOException {
+//        server.close();
+//    }
 }
