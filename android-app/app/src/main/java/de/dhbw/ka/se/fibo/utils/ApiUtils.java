d6dba5412b5a3611467145fd7512e055e807d43f
Jonas-Karl <98540934+Jonas-Karl@users.noreply.github.com>
2023-04-25 22:18:14 +0200

    Update android-app/app/src/main/java/de/dhbw/ka/se/fibo/utils/ApiUtils.java
    Co-authored-by: Jens Hausdorf <mail@jens-hausdorf.de>

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/utils/ApiUtils.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/utils/ApiUtils.java
@@ -19,7 +19,7 @@ public class ApiUtils {
         if (ActivityUtils.isEspressoTesting()) {
             return "http://localhost:8000";
         } else {
-            return "10.0.2.2:8000";
+            return "http://10.0.2.2:8000";
         }
     }
 
