a44682eff2c4554997f348b326cdd2774d5507c1
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2023-04-26 18:00:27 +0200

    Remove first_name & last_name

--- backend/api/models.py
+++ backend/api/models.py
@@ -36,6 +36,8 @@ class Item(models.Model):
 class FiboUser(AbstractUser):
     account = models.ManyToManyField(Account, blank=True)
     username = None
+    first_name = None
+    last_name = None
     email = models.EmailField(('email address'), unique=True)
 
     USERNAME_FIELD = 'email'
