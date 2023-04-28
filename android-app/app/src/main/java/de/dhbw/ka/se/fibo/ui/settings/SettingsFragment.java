8da379ea079d54b1ffe22cc87a89edc1e755d0b6
ChrisEwert <jc01.ewert@gmail.com>
2023-04-28 17:18:58 +0200

    OnCreateView and OnViewCreated

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
@@ -4,26 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.button.MaterialButton;
+
+import de.dhbw.ka.se.fibo.R;
 import de.dhbw.ka.se.fibo.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
   private FragmentSettingsBinding binding;
+  private MaterialButton deleteAccountButton;
 
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    SettingsViewModel settingsViewModel =
-        new ViewModelProvider(this).get(SettingsViewModel.class);
-
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-    TextView textView = binding.textSettings;
-    settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+    deleteAccountButton = binding.deleteAccount;
+
     return root;
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    initializeDeleteButton();
+  }
+
+  private void initializeDeleteButton() {
+
   }
 
   @Override
