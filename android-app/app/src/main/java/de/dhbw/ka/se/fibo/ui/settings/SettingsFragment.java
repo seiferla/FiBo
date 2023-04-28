ca20ec3a36b46807266897e573a41270aadecd45
ChrisEwert <jc01.ewert@gmail.com>
2023-04-28 17:46:34 +0200

    onError and onSuccess added

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
@@ -1,18 +1,25 @@
 package de.dhbw.ka.se.fibo.ui.settings;
 
+import static android.content.ContentValues.TAG;
+import static de.dhbw.ka.se.fibo.utils.ApiUtils.createAPIStringRequest;
+
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.button.MaterialButton;
 
 import de.dhbw.ka.se.fibo.ApplicationState;
@@ -23,13 +30,13 @@ import de.dhbw.ka.se.fibo.databinding.FragmentSettingsBinding;
 public class SettingsFragment extends Fragment {
 
   private FragmentSettingsBinding binding;
-  private MaterialButton deleteAccountButton;
+  private MaterialButton deleteUserButton;
 
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-    deleteAccountButton = binding.deleteAccount;
+    deleteUserButton = binding.deleteUser;
 
     return root;
   }
@@ -41,7 +48,7 @@ public class SettingsFragment extends Fragment {
   }
 
   private void initializeDeleteButton() {
-    deleteAccountButton.setOnClickListener(e -> openDialog());
+    deleteUserButton.setOnClickListener(e -> openDialog());
   }
 
   private void openDialog() {
@@ -50,8 +57,39 @@ public class SettingsFragment extends Fragment {
     builder.setTitle("Title");
     builder.setMessage("Message");
 
-    builder.setPositiveButton("Confirm",(dialog,which) -> {
+    Response.Listener<String> onSuccess = response -> {
+      Toast successToast = Toast.makeText(this.getContext(), "User deleted", Toast.LENGTH_LONG);
+      successToast.setGravity(Gravity.TOP, 0, 0);
+      successToast.show();
+      Log.i(TAG, "Successfully" + response);
+    };
 
+    Response.ErrorListener onError = error -> {
+      if (error.networkResponse != null) {
+        switch (error.networkResponse.statusCode) {
+          case 401:
+            Log.e(TAG, "Unauthorized");
+            break;
+          case 500:
+            Log.e(TAG, "Internal Server Error");
+            break;
+          default:
+            break;
+        }
+
+        Toast errorToast = Toast.makeText(this.getContext(), R.string.missing_token_error, Toast.LENGTH_LONG);
+        errorToast.show();
+        Log.e(TAG, String.valueOf(error));
+      } else {
+        Toast errorToast = Toast.makeText(this.getContext(), R.string.deleting_user_currently_unavailable, Toast.LENGTH_LONG);
+        errorToast.show();
+        Log.e(TAG, String.valueOf(error));
+      }
+    };
+
+    builder.setPositiveButton("Confirm",(dialog,which) -> {
+      String url = "/users/delete/";
+      StringRequest stringRequest = createAPIStringRequest(url, Request.Method.DELETE, null, onSuccess, onError);
 
       ApplicationState.getInstance(this.getContext()).clearAuthorization();
       Intent i = new Intent(this.getActivity(), LoginActivity.class);
