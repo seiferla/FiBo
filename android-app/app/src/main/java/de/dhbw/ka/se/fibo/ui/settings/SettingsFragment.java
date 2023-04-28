2b32297a959fb054e3ee3593c7f90947ed4c7800
ChrisEwert <jc01.ewert@gmail.com>
2023-04-28 15:46:44 +0200

    Change onCreateView in SettingsFragment

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
@@ -6,25 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import de.dhbw.ka.se.fibo.R;
+import de.dhbw.ka.se.fibo.databinding.FragmentAddingBinding;
 import de.dhbw.ka.se.fibo.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
   private FragmentSettingsBinding binding;
 
-  public View onCreateView(@NonNull LayoutInflater inflater,
-      ViewGroup container, Bundle savedInstanceState) {
-    SettingsViewModel settingsViewModel =
-        new ViewModelProvider(this).get(SettingsViewModel.class);
-
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//    SettingsViewModel settingsViewModel =
+//        new ViewModelProvider(this).get(SettingsViewModel.class);
+//
+//    binding = FragmentSettingsBinding.inflate(inflater, container, false);
+//    View root = binding.getRoot();
+//
+//    TextView textView = binding.textSettings;
+//    settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//    return root
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+    View view = binding.getRoot();
 
-    TextView textView = binding.textSettings;
-    settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-    return root;
+    return view;
   }
 
   @Override
