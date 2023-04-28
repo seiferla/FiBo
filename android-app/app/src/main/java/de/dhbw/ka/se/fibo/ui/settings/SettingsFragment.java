16d53d355243429f20113fc51566cbdfd89d0f57
ChrisEwert <jc01.ewert@gmail.com>
2023-04-28 15:47:12 +0200

    Add onViewCreated in SettingsFragment

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
@@ -36,6 +36,31 @@ public class SettingsFragment extends Fragment {
   }
 
   @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+//    initializeButtons();
+  }
+
+  //        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+  //            builder.setCancelable(true);
+  //            builder.setTitle("Title");
+  //            builder.setMessage("Message");
+  //            builder.setPositiveButton("Confirm",
+  //                    new DialogInterface.OnClickListener() {
+  //                        @Override
+  //                        public void onClick(DialogInterface dialog, int which) {
+  //                        }
+  //                    });
+  //            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+  //                @Override
+  //                public void onClick(DialogInterface dialog, int which) {
+  //                }
+  //            });
+  //
+  //            AlertDialog dialog = builder.create();
+  //            dialog.show();
+
+  @Override
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
