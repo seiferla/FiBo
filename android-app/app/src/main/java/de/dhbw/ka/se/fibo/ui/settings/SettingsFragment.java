6ae38aa5b172508982219600a7f9343d5c2811b1
ChrisEwert <jc01.ewert@gmail.com>
2023-04-28 16:11:20 +0200

    Revert changes

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/settings/SettingsFragment.java
@@ -6,13 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
-
-import de.dhbw.ka.se.fibo.R;
-import de.dhbw.ka.se.fibo.databinding.FragmentAddingBinding;
 import de.dhbw.ka.se.fibo.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -20,45 +15,16 @@ public class SettingsFragment extends Fragment {
   private FragmentSettingsBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//    SettingsViewModel settingsViewModel =
-//        new ViewModelProvider(this).get(SettingsViewModel.class);
-//
-//    binding = FragmentSettingsBinding.inflate(inflater, container, false);
-//    View root = binding.getRoot();
-//
-//    TextView textView = binding.textSettings;
-//    settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//    return root
+    SettingsViewModel settingsViewModel =
+        new ViewModelProvider(this).get(SettingsViewModel.class);
+
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
-    View view = binding.getRoot();
+    View root = binding.getRoot();
 
-    return view;
+    TextView textView = binding.textSettings;
+    settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+    return root;
   }
-
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-//    initializeButtons();
-  }
-
-  //        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-  //            builder.setCancelable(true);
-  //            builder.setTitle("Title");
-  //            builder.setMessage("Message");
-  //            builder.setPositiveButton("Confirm",
-  //                    new DialogInterface.OnClickListener() {
-  //                        @Override
-  //                        public void onClick(DialogInterface dialog, int which) {
-  //                        }
-  //                    });
-  //            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-  //                @Override
-  //                public void onClick(DialogInterface dialog, int which) {
-  //                }
-  //            });
-  //
-  //            AlertDialog dialog = builder.create();
-  //            dialog.show();
 
   @Override
   public void onDestroyView() {
