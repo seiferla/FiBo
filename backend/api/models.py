34e3b16ffc7d5c782a49da23b5c6f3341920beba
jens1o <mail@jens-hausdorf.de>
2023-05-09 17:23:08 +0200

    FiBo-282: Add adaptions to backend to make feature work(?)

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,14 +8,15 @@ class Place(models.Model):
     name = models.CharField(max_length=55)
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=55, unique=True)
-
-
 class Account(models.Model):
     name = models.CharField(max_length=55)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 
 class Cashflow(models.Model):
@@ -24,14 +25,16 @@ class Cashflow(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    place = models.ForeignKey(Place, on_delete=models.DO_NOTHING, blank=True, null=True)
+    place = models.ForeignKey(
+        Place, on_delete=models.DO_NOTHING, blank=True, null=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 
 class Item(models.Model):
     name = models.CharField(max_length=55)
     amount = models.IntegerField()
-    cashflow = models.ForeignKey(Cashflow, on_delete=models.CASCADE, blank=True, null=True)
+    cashflow = models.ForeignKey(
+        Cashflow, on_delete=models.CASCADE, blank=True, null=True)
     value = models.DecimalField(max_digits=6, decimal_places=2)
 
 
