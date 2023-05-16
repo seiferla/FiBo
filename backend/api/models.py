369e849862a5d098a016bf0a3face74540f8b9b3
Markus <markus.goetz17@gmail.com>
2023-05-16 17:28:57 +0200

    FiB0-197: create custom UserModel

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -35,6 +36,31 @@ class Item(models.Model):
     value = models.DecimalField(max_digits=6, decimal_places=2)
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class FiboUser(AbstractUser):
     username = None
     first_name = None
@@ -42,6 +68,7 @@ class FiboUser(AbstractUser):
     is_staff = None
     account = models.ManyToManyField(Account, blank=True)
     email = models.EmailField(('email address'), unique=True)
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
