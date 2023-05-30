3157dca81810a462acde0f7049cb3d418c1c5397
ChrisEwert <jc01.ewert@gmail.com>
2023-05-30 12:32:19 +0200

    add functioning test that checks that you can not use a future date

--- android-app/app/src/androidTest/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragmentTest.java
+++ android-app/app/src/androidTest/java/de/dhbw/ka/se/fibo/ui/adding/AddingFragmentTest.java
@@ -4,29 +4,42 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static de.dhbw.ka.se.fibo.TestMatchers.hasTextInputLayoutErrorText;
 
 import android.content.Context;
+import android.view.View;
 
 import androidx.navigation.Navigation;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.matcher.RootMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.google.android.material.internal.CheckableImageButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import de.dhbw.ka.se.fibo.MainActivity;
 import de.dhbw.ka.se.fibo.R;
@@ -273,5 +286,60 @@ public class AddingFragmentTest {
 
         onView(withId(R.id.address_text_layout))
                 .check(matches(hasTextInputLayoutErrorText("")));
+    }
+
+    @Test
+    public void testFutureDateNotPossible() {
+        DateTimeFormatter full = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        String today_full = full.format(now);
+
+        onView(withId(R.id.date_layout))
+                .perform(AddingFragmentTest.clickIcon(true));
+
+        onView(withId(com.google.android.material.R.id.confirm_button))
+                .perform(click());
+
+        onView(withId(R.id.date_text)).check(matches(withText(today_full)));
+
+        onView(withId(R.id.date_layout))
+                .perform(AddingFragmentTest.clickIcon(true));
+
+        onView(withId(com.google.android.material.R.id.mtrl_picker_header_toggle))
+                .perform(click());
+
+        onView(withText("30.05.23"))
+                .perform(replaceText("31.12.2999"));
+
+        onView(withText("31.12.2999"))
+                .perform(closeSoftKeyboard());
+
+        onView(withId(com.google.android.material.R.id.confirm_button))
+                .check(matches(isNotEnabled()));
+    }
+
+
+    public static ViewAction clickIcon(boolean isEndIcon) {
+        return new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(TextInputLayout.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Clicks the end or start icon";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextInputLayout item = (TextInputLayout) view;
+                // Reach in and find the icon view since we don't have a public API to get a reference to it
+                CheckableImageButton iconView =
+                        item.findViewById(isEndIcon ? com.google.android.material.R.id.text_input_end_icon : com.google.android.material.R.id.text_input_start_icon);
+                iconView.performClick();
+            }
+        };
     }
 }
