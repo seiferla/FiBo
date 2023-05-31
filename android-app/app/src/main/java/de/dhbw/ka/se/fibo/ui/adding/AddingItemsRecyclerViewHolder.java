2d7ac168a6d03db1b5a505d19ed8720ca0334a17
Jonas-Karl <jonas.karl.rade@gmail.com>
2023-05-31 20:12:28 +0200

    clean up recycler_view_item_row fill in ViewHolder

--- android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingItemsRecyclerViewHolder.java
+++ android-app/app/src/main/java/de/dhbw/ka/se/fibo/ui/adding/AddingItemsRecyclerViewHolder.java
@@ -1,4 +1,30 @@
 package de.dhbw.ka.se.fibo.ui.adding;
 
-public class AddingItemsRecyclerViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textview.MaterialTextView;
+
+import de.dhbw.ka.se.fibo.R;
+
+public class AddingItemsRecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    final MaterialCardView materialCard;
+    final TextView detailsButton;
+    final MaterialTextView itemName;
+    final MaterialTextView itemDate;
+    final MaterialTextView itemValue;
+
+    public AddingItemsRecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        materialCard = itemView.findViewById(R.id.adding_items_row_material_card_view_container);
+        detailsButton = itemView.findViewById(R.id.adding_items_row_imageViewDrawable);
+        itemName = itemView.findViewById(R.id.adding_items_row_item_name);
+        itemDate = itemView.findViewById(R.id.adding_items_row_date);
+        itemValue = itemView.findViewById(R.id.adding_items_row_item_value);
+    }
 }
