f66f9629bd75309200c92a4729eb50af459e3c3d
Jonas-Karl <jonas.karl.rade@gmail.com>
2023-06-03 16:56:27 +0200

    FiBo-303: remove star import

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import *
+from .models import Account, Cashflow, Store, Private, Item, FiboUser, Category
 
 
 class AccountSerializer(ModelSerializer):
