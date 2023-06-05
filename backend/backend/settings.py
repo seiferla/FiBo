bc143ba63b23ad146b783301f1ca548d473672cb
Markus <markus.goetz17@gmail.com>
2023-06-05 21:32:05 +0200

    FiBo-344: set Debug mode to false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '10.0.2.2', 'localhost', 'fibo.gtzcloud.de']
 
